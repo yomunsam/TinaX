5b2b7cb807d29161af4be47c969ec54dfc1d6054
大魔王有木桑 <yomunsam@outlook.com>
2020-12-04 16:33:46 +0800

    update i18n

--- Examples/E01_HelloWorld_CSharp/Assets/App/Scripts/UI/CounterScreen.cs
+++ Examples/E01_HelloWorld_CSharp/Assets/App/Scripts/UI/CounterScreen.cs
@@ -11,17 +11,23 @@ namespace Nekonya.Example
         public Text txt_counter { get; set; }
         public Button btn_click { get; set; }
 
-        [Inject]
-        public II18N m_I18N { get; set; }
+        [Inject] public II18N m_I18N { get; set; }
 
         private BindableProperty<int> m_currentCounter = new BindableProperty<int>(-1);
 
+        private ILocalizerGroup L;
+
         public override void Awake()
         {
+            L = m_I18N.GetGroup("common");
+
             m_currentCounter.OnValueChanged((_, newValue) =>
             {
-                txt_counter.text = string.Format(
-                    m_I18N.GetText("txt_cur_counter", "common", "Current count: {0}"), newValue);
+                // //Old
+                //txt_counter.text = string.Format(
+                //    m_I18N.GetText("txt_cur_counter", "common", "Current count: {0}"), newValue);
+
+                txt_counter.text = L["txt_cur_counter", newValue];
             });
             
 
